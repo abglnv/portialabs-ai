5fb3223bace39eb1a4fa3c05b13a2a64f76ceb4f
rendizi <baglanov.a0930@gmail.com>
2025-04-13 08:14:59 +0500

    fix

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 from app.routes import auth, services, exploits, reports
 from app.services.cron import run_cron
-import uvicorn
 # run_cron()
 
 load_dotenv()
@@ -14,5 +13,3 @@ app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(services.router, prefix="/services", tags=["Services"])
 app.include_router(exploits.router, prefix="/exploits", tags=["Exploits"])
 app.include_router(reports.router, prefix="/reports", tags=["Reports"])
-
-uvicorn.run(app)
